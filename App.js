50b1f9be80fa15db918e261a9a7188624cfe7da5
Arun Mahajan <153971993+Ghasita@users.noreply.github.com>
2024-05-14 16:49:33 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,21 @@
+
+import './App.css';
+import Try from './Comp/Try';
+import {useState} from 'react'
+
+
+function App() {
+  
+ 
+  return (
+    
+     
+     <div ><Try/>
+   
+    
+     </div> 
+  )
+   
+}
+
+export default App;
